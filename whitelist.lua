125c193d8b8b87e38c6c94079eeee075f37cc52c
ZloppyS <120696067+ZloppyS@users.noreply.github.com>
2023-07-05 20:48:08 -0400

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,11 +1,1 @@
-return {
-    "BD575417-0AA5-44B0-9587-802C9a805136", -- me (byfron hater)
-}
-
-local Blacklist {
-    "0AADBC8D-A251-4E9D-8ECE-D07992D579ED", -- Mont
-}
-
-if Blacklist [game:GetService("RbxAnalyticsService"):GetClientId()] then
-    game.Players.LocalPlayer:Kick("blacklisted? josiah boney momento")
-end
+print("Nigga please why you looking here")
